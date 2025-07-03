fa8354abf3b50a8e112189cb36d68d6bba772274
Użytkownik <Uytkownik@KAMIL-KOMPUTER>
2025-07-03 17:15:54 +0200

    Wstępna definicja klasy Vertex, Edge i Graph.

--- TDVRP/Graph.cs
+++ TDVRP/Graph.cs
@@ -6,8 +6,42 @@ using System.Threading.Tasks;
 
 namespace TDVRP;
 
+class Vertex
+{
+    double Order;
+    double ServiceTime;
+    List<Edge> Edges;
+
+    Vertex(double order, double serviceTime)
+    {
+        Order = order;
+        ServiceTime = serviceTime;
+        Edges = new List<Edge>();
+    }
+
+    Vertex(): this(0, 0) { }
+}
+
+class Edge
+{
+    Vertex End;
+    double Distance;
+    //double Speed(uint instant);
+}
+
 class Graph
 {
+    List<Vertex> vertices;
 
-    
+    Graph()
+    {
+        vertices = new List<Vertex>();
+    }
+
+    /* Manages construction and yields a graph whose layout cannot be modified from outside 
+       of the class. */
+    public class Builder
+    {
+
+    }
 }
