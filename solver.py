57f59f66bf907453afb8d40299791bd4fe3a9f99
kando-123 <100385385+kando-123@users.noreply.github.com>
2025-08-29 12:49:51 +0200

    Add files via upload
    Clusterization was added

--- solver.py
+++ solver.py
@@ -33,6 +33,97 @@ class Vertex:
 
 recent_modification = None
 
+class Cluster:
+    def __init__(self, v_key, task):
+        self.vertices = [v_key]
+        self.max_load = task.max_load
+        self.graph = task.graph
+        self.load = self.graph[v_key].order
+    
+    def mergeable(self, cluster) -> bool:
+        return self.load + cluster.load <= self.max_load
+    
+    def incorporate(self, cluster) -> bool:
+        self.vertices.extend(cluster.vertices)
+        self.load += cluster.load
+        cluster.vertices.clear( )
+        cluster.load = 0
+    
+    def minimum_metric(lhs, rhs) -> float:
+        min_dist = float('inf')
+        for k1 in lhs.vertices:
+            (x1, y1) = self.graph[k1].coords
+            for k2 in rhs.vertices:
+                (x2, y2) = self.graph[k2].coords
+                dist = math.hypot(x2 - x1, y2 - y1)
+                if dist < min_dist:
+                    min_dist = dist
+        return min_dist
+    
+    def maximum_metric(lhs, rhs) -> float:
+        max_dist = float('-inf')
+        for k1 in lhs.vertices:
+            (x1, y1) = lhs.graph[k1].coords
+            for k2 in rhs.vertices:
+                (x2, y2) = rhs.graph[k2].coords
+                dist = math.hypot(x2 - x1, y2 - y1)
+                if dist > max_dist:
+                    max_dist = dist
+        return max_dist
+    
+    def centroid_metric(lhs, rhs) -> float:
+        (x1, y1) = (0, 0)
+        for k1 in lhs.vertices:
+            (x, y) = self.graph[k1].coords
+            x1 += x
+            y1 += y
+        x1 /= len(lhs.vertices)
+        y1 /= len(lhs.vertices)
+        (x2, y2) = (0, 0)
+        for k2 in lhs.vertices:
+            (x, y) = self.graph[k1].coords
+            x2 += x
+            y2 += y
+        x2 /= len(rhs.vertices)
+        y2 /= len(rhs.vertices)
+        return math.hypot(x2 - x1, y2 - y1)
+    
+    def weighted_metric(lhs, rhs) -> float:
+        (x1, y1) = (0, 0)
+        for k in lhs.vertices:
+            (x, y) = self.graph[k].coords
+            q = self.graph[k].order
+            x1 += x * q
+            y1 += y * q
+        x1 /= len(lhs.load)
+        y1 /= len(lhs.load)
+        (x2, y2) = (0, 0)
+        for k in lhs.vertices:
+            (x, y) = self.graph[k].coords
+            q = self.graph[k].order
+            x2 += x * q
+            y2 += y * q
+        x2 /= len(rhs.load)
+        y2 /= len(rhs.load)
+        return math.hypot(x2 - x1, y2 - y1)
+    
+    metric = maximum_metric
+    
+    def set_metric(str):
+        if str == 'minimum' or str == 'min':
+            metric = minimum_metric
+        elif str == 'maximum' or str == 'max':
+            metric = maximum_metric
+        elif str == 'centroid':
+            metric = centroid_metric
+        elif str == 'weighted':
+            metric = centroid_metric
+        else:
+            raise ValueError('Wrong metric name')
+    
+    def distance(self, other) -> float:
+        return Cluster.metric(self, other)
+
 class Task:
     def __init__(self, source):
         with open(source, 'r') as file:
@@ -91,37 +182,56 @@ class Task:
             full_path.append(path[i])
         return full_path, start_time   # Actually, it's become the end time :) 
     
-    def failover_solution(self):
-        sol = [ ]
-        load = [ ]
-        time = [ ]
-        for _ in range(self.n_vehicles):
-            sol.append( [ (self.depot, 0) ] )
-            load.append(0)
-        vertices = PriorityQueue( )   # Greatest order first
-        for k, v in self.graph.items( ):
-            if k != self.depot:
-                vertices.put( (-v.order, k) )
-        while not vertices.empty( ):
-            _, vertex = vertices.get( )
-            order = self.graph[vertex].order
-            vehicles = PriorityQueue( )
-            for i in range(self.n_vehicles):
-                vehicles.put( (load[i], i) )   # Least loaded first
-            _, vehicle = vehicles.get( )
-            if load[vehicle] + order > self.max_load:
-                raise Exception('Initial solution generation failed')
+    def clusterization_solution(self):
+        clusters = [ Cluster(key, self) for key in self.graph if key != self.depot ]
+        while len(clusters) > self.n_vehicles:
+            # Queue will store pairs (i, j) of clusters, i < j,
+            #     from the least distant to the most
+            queue = PriorityQueue( )
+            for i in range(0, len(clusters)):
+                c1 = clusters[i]
+                for j in range(i + 1, len(clusters)):
+                    c2 = clusters[j]
+                    entry = c1.distance(c2), (i, j)
+                    queue.put(entry)
+            # Merge the two closest clusters,
+            #    on condition they will not exceed the max load
+            while not queue.empty( ):
+                _, (i, j) = queue.get( )
+                c1 = clusters[i]
+                c2 = clusters[j]
+                if c1.mergeable(c2):
+                    c1.incorporate(c2)
+                    clusters.pop(j)
+                    break
             else:
-                load[vehicle] += order
-                sol[vehicle].append((vertex, order))
-        for path in sol:
+                raise Exception('Clusterization failed!')
+        # Transform the clusters into paths
+        paths = [ ]
+        loads = [ ]
+        times = [ ]
+        for cluster in clusters:
+            path = [ (self.depot, 0) ]
+            while len(cluster.vertices) > 0:
+                (x0, y0) = self.graph[path[-1][0]].coords
+                min_dist, min_idx = float('inf'), None
+                for idx, vtx in enumerate(cluster.vertices):
+                    (x1, y1) = self.graph[vtx].coords
+                    dist = math.hypot(x1 - x0, y1 - y0)
+                    if dist < min_dist:
+                        min_dist = dist
+                        min_idx = idx
+                next = cluster.vertices.pop(min_idx)
+                node = (next, self.graph[next].order)
+                path.append(node)
             path.append((self.depot, 0))
-        for i, path in enumerate(sol):
-            sol[i], dur = self.fill(path, 0)
-            time.append(dur)
-        return sol, load, time
+            path, time = self.fill(path, 0)
+            paths.append(path)
+            loads.append(cluster.load)
+            times.append(time)
+        return paths, loads, times
     
-    def initial_solution(self):
+    def greedy_solution(self):
         sol = [ ]
         load = [ ]
         time = [ ]
@@ -158,6 +268,36 @@ class Task:
             return sol, load, time
         else:
             raise Exception('Initial solution generation failed')
+    
+    def failover_solution(self):
+        sol = [ ]
+        load = [ ]
+        time = [ ]
+        for _ in range(self.n_vehicles):
+            sol.append( [ (self.depot, 0) ] )
+            load.append(0)
+        vertices = PriorityQueue( )   # Greatest order first
+        for k, v in self.graph.items( ):
+            if k != self.depot:
+                vertices.put((-v.order, k))
+        while not vertices.empty( ):
+            _, vertex = vertices.get( )
+            order = self.graph[vertex].order
+            vehicles = PriorityQueue( )
+            for i in range(self.n_vehicles):
+                vehicles.put( (load[i], i) )   # Least loaded first
+            _, vehicle = vehicles.get( )
+            if load[vehicle] + order > self.max_load:
+                raise Exception('Initial solution generation failed')
+            else:
+                load[vehicle] += order
+                sol[vehicle].append((vertex, order))
+        for path in sol:
+            path.append((self.depot, 0))
+        for i, path in enumerate(sol):
+            sol[i], dur = self.fill(path, 0)
+            time.append(dur)
+        return sol, load, time
     
     def evaluate_path(self, path):
         travel_time = 0
@@ -278,36 +418,40 @@ class Task:
 def main( ):
     task = Task(sys.argv[1])
     
-    # sol, load, times = task.initial_solution( )
-    for _ in range(10):
-        try:
-            sol, load, times = task.initial_solution( )
-            print('break')
-            break
-        except Exception as e:
-            print(e)
-            print('exception!')
-            pass
-    else:
-        try:
-            sol, load = task.failover_solution( )
-        except:
-            print('Failure')
-            exit(1)
+    paths, loads, times = None, None, None
+    
+    try:
+        paths, loads, times = task.clusterization_solution( )
+    except:
+        for _ in range(10):
+            try:
+                paths, loads, times = task.greedy_solution( )
+                print('break')
+                break
+            except Exception as e:
+                print(e)
+                print('exception!')
+                pass
+        else:
+            try:
+                paths, loads, times = task.failover_solution( )
+            except:
+                print('Failure')
+                exit(1)
     
     # The algorithm for determining the initial solution can be upgraded later.
     # Nevertheless, at this point there is SOME initial solution to start with.
     
     N_ITER = 10_000
     TEMP   =    100
-    best_sol, best_eval = sol, task.evaluate_solution(sol)
+    best_sol, best_eval = paths, task.evaluate_solution(paths)
     curr_sol, curr_eval = best_sol, best_eval
     scores = [best_eval]
     
     for i in range(N_ITER):
         t = TEMP / float(i + 1)
         
-        cand_sol  = task.neighboring_solution(sol)
+        cand_sol  = task.neighboring_solution(paths)
         cand_eval = task.evaluate_solution(cand_sol)
         
         if cand_eval < best_eval or random.random( ) < math.exp((curr_eval - cand_eval) / t):
